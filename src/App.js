11820b676ca3b883db439baa823a2655e3fb59a3
unknown <Devendra Prashin@PC1678.nordic.local>
2021-09-21 10:54:09 +0200

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Txt Forum</h1>
+      <h4>
+        <span className="material-icons">loyalty</span>
+      </h4>
+    </div>
+  );
+}
+
+export default App;
