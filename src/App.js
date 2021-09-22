f24a28dd07770716fb379b16b10daebed0a28540
unknown <Devendra Prashin@PC1678.nordic.local>
2021-09-22 15:58:14 +0200

    Apply css to the layout

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ import {
 } from 'react-router-dom'
 import Login from './components/Login/Login'
 import NotFound from './components/NotFound/NotFound'
+import AppContainer from './hoc/AppContainer'
 
 
 function App() {
   return (
     <BrowserRouter> 
     <div className="App">
-      <h1>React Txt Forum</h1>
-      <h4>
-        <span className="material-icons">loyalty</span>
-      </h4>
+      <AppContainer>
+        <h1>React Txt Forum</h1>
+        <h4>
+          <span className="material-icons">loyalty</span>
+        </h4>
+      </AppContainer>
       <Switch>
         <Route path="/" exact component={ Login }/>
         <Route path="/*" component={NotFound} />
