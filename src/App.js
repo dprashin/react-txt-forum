3558a83f697e0925b9645ea68469a34d956ee6d2
unknown <Devendra Prashin@PC1678.nordic.local>
2021-09-22 15:15:05 +0200

    Cnfigure route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import './App.css';
+import './App.css'
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
+import Login from './components/Login/Login'
+import NotFound from './components/NotFound/NotFound'
+
 
 function App() {
   return (
+    <BrowserRouter> 
     <div className="App">
       <h1>React Txt Forum</h1>
       <h4>
         <span className="material-icons">loyalty</span>
       </h4>
+      <Switch>
+        <Route path="/" exact component={ Login }/>
+        <Route path="/*" component={NotFound} />
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
