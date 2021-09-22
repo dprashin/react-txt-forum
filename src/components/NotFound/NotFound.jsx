c543cf29fe6af6d387a9208b20a6aa94eaebb209
unknown <Devendra Prashin@PC1678.nordic.local>
2021-09-22 15:15:32 +0200

    Create NotFound component

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+const NotFound = () => {
+    return (
+        <main>
+            <h4>Page Not Found.</h4>
+            <Link to= "/">Go to Start Page.</Link>
+        </main>
+    )
+}
+export default NotFound
