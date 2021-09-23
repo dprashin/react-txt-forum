e3c9efe865c1379c8af6d4b2381f30ef2faadd4f
unknown <Devendra Prashin@PC1678.nordic.local>
2021-09-23 18:02:41 +0200

    setup register form, link input to state, create a submission handler

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,20 +1,41 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
 import AppContainer from "../../hoc/AppContainer";
 
 const Register = () => {
   const { loggedIn } = useSelector((state) => state.sessionReducer);
+
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+    avatar: "",
+  });
+
+  const onRegisterSubmit = (event) => {
+    event.preventDefault();
+    console.log("Register.onRegisterSubmit()", user);
+  };
+
+  const onInputChange = event => {
+    setUser({
+      ...user,
+      [event.target.id]: event.target.value
+    })
+  }
+
   return (
     <AppContainer>
       {loggedIn && <Redirect to="/timeline" />}
-      <form className="mb-3">
+      <form className="mb-3" onSubmit={onRegisterSubmit}>
         <h1> Register for Text Forum</h1>
         <p>Complete the form to create your account. </p>
         <div className="mb-3">
           <label htmlFor="username" className="form-label">
             Choose a username
           </label>
-          <input type="text" id="username" className="form-control" />
+          <input type="text" id="username" className="form-control" onChange= { onInputChange } />
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
@@ -25,6 +46,7 @@ const Register = () => {
             id="password"
             className="form-control"
             placeholder="*********"
+            onChange= { onInputChange }
           />
         </div>
         <div className="mb-3">
@@ -36,6 +58,7 @@ const Register = () => {
             id="confirmPassword"
             className="form-control"
             placeholder="*********"
+            onChange= { onInputChange }
           />
         </div>
         <button className="btn btn-success btn-lg">Register</button>
