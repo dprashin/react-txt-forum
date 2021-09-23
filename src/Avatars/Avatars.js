6a28dd47a1276d1d81b7107fa0cd7248ecfc3696
unknown <Devendra Prashin@PC1678.nordic.local>
2021-09-23 19:18:16 +0200

    Add styling in avatars

--- src/Avatars/Avatars.js
+++ src/Avatars/Avatars.js
@@ -1,22 +1,47 @@
-import { useEffect } from "react"
-import { AvatarsAPI } from "./AvatarsAPI"
+import { useEffect, useState } from "react";
+import { AvatarsAPI } from "./AvatarsAPI";
+import styles from './Avatars.module.css'
 
 const Avatars = () => {
+  const [state, setState] = useState({
+    avatars: [],
+    fetching: true,
+    selectedAvatar: -1,
+    error: "",
+  });
 
-    useEffect(() => { //emulates componentDidMount
-        AvatarsAPI.getAvatars()
-        .then(avatars => {
-            console.log('Avatars.getAvatars()', avatars);
-        })
-        .catch(error => {
-            console.error('Avatars.getAvatars()', error);
-        })
-    }, [])
+  useEffect(() => {
+    //emulates componentDidMount
+    AvatarsAPI.getAvatars()
+      .then((avatars) => {
+        // console.log('Avatars.getAvatars()', avatars);
+        setState({
+          ...state,
+          error: "",
+          fetching: false,
+          avatars, //objet literal. Actuall could be written as: avatars: avatars
+        });
+      })
+      .catch((error) => {
+        // console.error('Avatars.getAvatars()', error);
+        setState({
+          ...state,
+          error: error.message,
+          fetching: false,
+        });
+      });
+  }, []);
 
-    return (
-        <section>
-            <h4>Choose your avatar</h4>
-        </section>
-    )
-}
-export default Avatars
+  return (
+    <section>
+      <h4>Choose your avatar</h4>
+      {state.fetching && <p>Avatars are loading ...</p>}
+      <div className= {styles.Avatars}>
+        {state.avatars.map((avatar) => (
+          <img  className= { styles.AvatarsImage } key={avatar.id} src={avatar.url} alt="Avatar" />
+        ))}
+      </div>
+    </section>
+  );
+};
+export default Avatars;
